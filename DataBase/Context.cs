01b6ad5793832f2a0346c5e747566aed024ad4b5
Wiktor <wiktor.szczeszek99.other@gmail.com>
2021-10-25 22:31:58 +0200

    Dodaj pliki projektów.

---
+++ DataBase/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataBase
+{
+    public class Context : DbContext
+    {
+        //public DbSet<Test1DataModel> Test1 { get; set; }
+        //public DbSet<Test2DataModel> Test2 { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"  
+                Server=localhost;Database=TestDB;Trusted_Connection=True;"
+            );
+        }
+    }
+}
