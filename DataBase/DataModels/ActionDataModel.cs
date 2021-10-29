1c1b50bd8f755e17d26e54aefc7a1c6935e4605a
Wiktor Szczeszek <wiktor.szczeszek@wercom.pl>
2021-10-29 16:16:20 +0200

    Update data models #1

--- DataBase/DataModels/ActionDataModel.cs
+++ DataBase/DataModels/ActionDataModel.cs
@@ -5,31 +5,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataBase.DataModels
 {
-    [Table("Tasks")]
-    public class TaskDataModel
+    [Table("Action")]
+    public class ActionDataModel
     {
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [Column("Creator")]
-        public WorkerDataModel Creator { get; set; }
+        [Column("Task")]
+        public TaskDataModel Task { get; set; }
 
         [Column("Excecutor")]
         public WorkerDataModel Excecutor { get; set; }
-
-        [Column("ExcecutorType")]
-        public WorkerType? ExcecutorType { get; set; }
 
         public State State { get; set; }
 
         public string Description { get; set; }
 
         [Required]
-        public DateTime StartDatePlanned { get; set; }
+        public DateTime StartDateReal { get; set; }
 
         [Required]
-        public DateTime EndDatePlanned { get; set; }
+        public DateTime EndDateReal { get; set; }
 
         [Timestamp]
         public byte[] Timestamp { get; set; }
