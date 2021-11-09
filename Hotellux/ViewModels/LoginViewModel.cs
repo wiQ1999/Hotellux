b583694a4536f1d87e7f842556326aff70083c6e
Wiktor <wiktor.szczeszek99.other@gmail.com>
2021-11-09 22:51:43 +0100

    Kontunuacja modeli widoków

---
+++ Hotellux/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DataBase.DataModels;
+using Hotellux.Repositories;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hotellux.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private LoginDataModel _loginModel;
+        private LoginRepository _loginRepository = new();
+
+        //public WorkerDataModel 
+
+        public LoginViewModel()
+        {
+            _loginModel = new LoginDataModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
