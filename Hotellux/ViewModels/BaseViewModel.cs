7f1028ad6cf209b1493cd4053f96e5d6fb24872e
Wiktor <wiktor.szczeszek99.other@gmail.com>
2021-12-02 22:49:22 +0100

    Modyfikacja walidatora danych

--- Hotellux/ViewModels/BaseViewModel.cs
+++ Hotellux/ViewModels/BaseViewModel.cs
@@ -1,15 +1,50 @@
 ﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Hotellux.ViewModels
 {
-    public abstract class BaseViewModel : INotifyPropertyChanged
+    public abstract class BaseViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
     {
+        private readonly Dictionary<string, List<string>> _propertyErrors = new();
+
         public abstract string ViewModelName { get; }
+
+        public bool HasErrors => _propertyErrors.Any();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName) => _propertyErrors.GetValueOrDefault(propertyName, null);
+
+        public void AddError(string propertyName, string errorMessage)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+                _propertyErrors.Add(propertyName, new List<string>());
+
+            _propertyErrors[propertyName].Add(errorMessage);
+            OnErrorChanged(propertyName);
+        }
+
+        public void ClearErrors([CallerMemberName] string propertyName = null)
+        {
+            if (_propertyErrors.Remove(propertyName))
+                OnErrorChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnErrorChanged([CallerMemberName] string propertyName = null)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
     }
 }
